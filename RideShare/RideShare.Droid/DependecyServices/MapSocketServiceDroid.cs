3c9d14a02fc2d6b61ef2e62e7e987034be4e9bef
nhettiarachchi <nhettiarachchi@CMLNHETTIARACHC>
2016-07-04 20:01:15 +0530

    updates relating to map

---
+++ RideShare/RideShare.Droid/DependecyServices/MapSocketServiceDroid.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RideShare.SharedInterfaces;
+using Quobject.SocketIoClientDotNet.Client;
+
+[assembly: Xamarin.Forms.Dependency(typeof(RideShare.Droid.DependecyServices.MapSocketServiceDroid))]
+namespace RideShare.Droid.DependecyServices
+{
+    public class MapSocketServiceDroid : IMapSocketService
+    {
+        public event EventHandler MapCoordinateChanged;
+
+        public MapSocketServiceDroid()
+        {
+            var socket = IO.Socket("http://172.28.40.104:8079");
+            socket.On("coordinate_changed", MapUpdated);
+        }
+
+        private void MapUpdated(object obj)
+        {
+            MapCoordinateChanged(obj, null);
+        }
+    }
+}
