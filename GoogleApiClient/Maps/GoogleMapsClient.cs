aad5a35103db2daf5c39d62e38d424034f33e399
sithi1990 <sithi1990@live.com>
2016-07-10 12:17:15 +0530

    Google directions API client implementation in-progress

---
+++ GoogleApiClient/Maps/GoogleMapsClient.cs
@@ -0,0 +1,44 @@
+﻿using Common;
+using GoogleApiClient.Helpers;
+using GoogleApiClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApiClient.Maps
+{
+    public class GoogleMapsDirectionsClient
+    {
+
+       
+        public string ApiKey
+        {
+            get 
+            {
+                return "AIzaSyC4gJh0TBYqXX3H0PXXGdMJIQok4TivVZo";
+            }
+           
+        }
+
+        public GoogleMapsDirectionsClient()
+        {
+
+        }
+
+        public GetDirectionsResponse GetDirections(GetDirectionRequest request)
+        {
+            GoogleDirectionsApiUrlBuilder urlBuilder = new GoogleDirectionsApiUrlBuilder();
+            urlBuilder.ApiKey = this.ApiKey;
+            urlBuilder.AddWaypoints<Coordinate>(request.WayPoints);
+            urlBuilder.SourceCoordinates = request.SourceCoordinate;
+            urlBuilder.DestinationCoordinates = request.DestinationCoordinate;
+
+            HttpRequestHandler requestHandler = new HttpRequestHandler();
+            requestHandler.Url = urlBuilder.GetGeneratedUrl();
+            requestHandler.Method = "GET";
+            return requestHandler.SendRequest<GetDirectionsResponse>();
+        }
+    }
+}
---
+++ GoogleApiClient/Models/GetDirectionRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApiClient.Models
+{
+    public class GetDirectionRequest
+    {
+        private Coordinate sourceCoordinate = new Coordinate();
+
+        public Coordinate SourceCoordinate
+        {
+            get { return sourceCoordinate; }
+            set { sourceCoordinate = value; }
+        }
+
+        private Coordinate destinationCoordinate = new Coordinate();
+
+        public Coordinate DestinationCoordinate
+        {
+            get { return destinationCoordinate; }
+            set { destinationCoordinate = value; }
+        }
+
+        public List<Coordinate> WayPoints { get; set; }
+
+
+    }
+}
