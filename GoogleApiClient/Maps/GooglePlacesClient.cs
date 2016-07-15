22ccd05043cc085d04e29d6a8b97798e40d97fe4
bandarath <bandarath@gmail.com>
2016-07-15 14:17:49 +0530

    query prediction for text-based geographic searches

---
+++ GoogleApiClient/Maps/GooglePlacesClient.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using GoogleApiClient.Helpers;
+using GoogleApiClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApiClient.Maps
+{
+    class GooglePlacesClient
+    {
+        private string ApiKey
+        {
+            get 
+            {
+                return "AIzaSyCcgcdVESAmhvKP74MbgH2SDyuS63qqNtI";
+            }
+           
+        }
+
+        public GooglePlacesClient()
+        {
+
+        }
+
+        public GetPlacesResponse GetPlaces(String textSearch)
+        {
+            const string strAutoCompleteGoogleApi = "https://maps.googleapis.com/maps/api/place/queryautocomplete/json?key={0}&input={1}";
+            HttpRequestHandler requestHandler = new HttpRequestHandler();
+            requestHandler.Url = String.Format(strAutoCompleteGoogleApi, ApiKey, textSearch);
+            requestHandler.Method = "GET";
+            return requestHandler.SendRequest<GetPlacesResponse>();
+        }
+    }    
+}
---
+++ GoogleApiClient/Models/GetPlacesResponse.cs
@@ -0,0 +1,28 @@
+﻿using GoogleApiClient.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApiClient.Models
+{    
+    public class Prediction
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("place_id")]
+        public string Placeid { get; set; }
+
+    }
+    public class GetPlacesResponse
+    {
+        [JsonProperty("predictions")]
+        public IList<Prediction> Predictions { get; set; }
+    }
+}
