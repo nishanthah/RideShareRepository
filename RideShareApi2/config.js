fe7dfca3af5889e5ac0b8fd788a7f1afc6045c9f
sithi1990 <sithi1990@live.com>
2016-09-07 15:15:26 +0530

    Changed bd

--- RideShareApi2/config.js
+++ RideShareApi2/config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     'secret': 'vappsecret',
-    'database': 'mongodb://localhost:27017/vappnodedb'
+	'database': 'mongodb://admin:admin@jello.modulusmongo.net:27017/ra2vEbow'
+    //'database': 'mongodb://localhost:27017/vappnodedb'
 
 };
