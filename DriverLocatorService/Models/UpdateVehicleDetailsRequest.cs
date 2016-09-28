de283daf9ec0960ee2bda20e011be79d8e4a958a
vwijeratne <varunipw@gmail.com>
2016-09-28 12:12:54 +0530

    Adding new request and response files for user vehicle registration

---
+++ DriverLocatorService/Models/UpdateVehicleDetailsRequest.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverLocator.Models
+{
+    public class UpdateVehicleDetailsRequest
+    {
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("vehicleModel")]
+        public string VehicleModel { get; set; }
+
+        [JsonProperty("vehicleColor")]
+        public string VehicleColor { get; set; }
+
+        [JsonProperty("vehicleMaxPassengerCount")]
+        public int VehicleMaxPassengerCount { get; set; }
+
+        [JsonProperty("vehicleNumberPlate")]
+        public string VehicleNumberPlate { get; set; }
+
+        [JsonProperty("previousVehicleNumberPlate")]
+        public string PreviousVehicleNumberPlate { get; set; }
+ 
+    }
+}
