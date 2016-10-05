7b718b0c1a7767f7857216ad1e3e60f6212665c7
vwijeratne <varunipw@gmail.com>
2016-10-05 16:07:09 +0530

    Changing the UI of the vehicle registration page.

---
+++ DriverLocatorService/Models/UserVehicleDefinitionDataResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverLocator.Models
+{
+    public class UserVehicleDefinitionDataResponse : ResponseBase
+    {
+        [JsonProperty("userVehicleDefData")]
+        public ObservableCollection<VehicleDefinitionData> UserVehicleDefinitionData { get; set; }
+    }
+}
