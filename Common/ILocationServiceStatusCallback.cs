ebf1c7f7c661da2f8e6ab48f1adb477f537e73dc
sithi1990 <sithi1990@live.com>
2016-10-24 18:24:34 +0530

    Fixed the RiderNavigationView crash issue

---
+++ Common/ILocationServiceStatusCallback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum LocationServiceStatus
+    {        
+        OutOfService = 0,        
+        TemporarilyUnavailable = 1,
+        Available = 2
+    }
+    public interface ILocationServiceStatusCallback
+    {
+        void OnLocationStatusChange(LocationServiceStatus status);
+    }
+}
