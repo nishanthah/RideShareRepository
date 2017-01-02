47affb86329d5c73d77449e63f541b193abeeac4
johnatm <john.atm@gmail.com>
2017-01-02 15:24:47 +0530

    AuthenticationApiTSTest and RideShareApi2Test projects added

---
+++ AuthenticationApiTSTest/test/AuthenticationApiControllerTest.js
@@ -0,0 +1,3 @@
+/// <reference path="../Scripts/typings/mocha/mocha.d.ts" />
+//import ua = useraccount(".../AuthenticationApiTs/routes/AuthenticationApiController.ts") 
+//# sourceMappingURL=AuthenticationApiControllerTest.js.map
---
+++ AuthenticationApiTSTest/test/AuthenticationApiControllerTest.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../Scripts/typings/mocha/mocha.d.ts" />
+//import AuthhenticationAPIController from '../'; 
+
+import AuthhenticationAPIController from '../node_modules/test-type-script-api/';
+
+//import * as _ from 'test-type-script-api';
+
+describe('AuthhenticationAPIController', () => {
+    var newuser: AuthhenticationAPIController;
+
+  
+
+    describe('#useraccount', () => {
+        it('should create the user', () => {
+            var result: Object = newuser.adduser("TestUser");
+           
+        });
+    });
+});
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
